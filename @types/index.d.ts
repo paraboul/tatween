4dfed23edec680f224a1a24e4afb073ad0035a53
Anthony Catel <paraboul@gmail.com>
2021-09-12 12:55:16 +0200

    typescript definition: tatween returns a function with a completion callback

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -3,7 +3,7 @@ type EasingFunc = (k: number) => number;
 type animationBlock<T> = (...objs: T) => animationBlock<T> | void
 
 export function start() : void;
-export function tatween<T>(duration: number, easeing: EasingFunc, callback: animationBlock<T>, ...objs: T) : () => void;
+export function tatween<T>(duration: number, easeing: EasingFunc, callback: animationBlock<T>, ...objs: T) : (complete: () => void) => void;
 
 interface Ops {
     In: EasingFunc
